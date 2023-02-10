07617ceeab65f96cc1408db06f63defb163e6923
Keeano Gerald <keeanogerald@gmail.com>
2023-02-10 00:47:06 -0500

    Added cathc for invalid input for initialize

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -25,16 +25,21 @@ public class main {
 
 
     static void initializeArray(String userString) {
-        Player p1 = new Player(0, 0, "Up", "North");
-        int arraySize = Integer.parseInt(userString.substring(2, userString.length()));
-        System.out.println("Array size entered is: " + arraySize);
-        int[][] newBoard = new int[arraySize][arraySize];
-        for (int i = 0; i < arraySize; i++) {
-            for (int j = 0; j < arraySize; j++) {
-                newBoard[i][j] = 0;
+        int arraySize;
+        try {
+            arraySize = Integer.parseInt(userString.substring(2, userString.length()));
+            System.out.println("Array size entered is: " + arraySize);
+            Player p1 = new Player(0, 0, "Up", "North");
+            int[][] newBoard = new int[arraySize][arraySize];
+            for (int i = 0; i < arraySize; i++) {
+                for (int j = 0; j < arraySize; j++) {
+                    newBoard[i][j] = 0;
+                }
             }
+            printBoard(newBoard, arraySize, p1);
+        } catch (Exception e) {
+            System.out.println("Invalid input");
         }
-        printBoard(newBoard, arraySize, p1);
     }
 
     static void printBoard(int[][] board, int arraySize, Player p1) {
