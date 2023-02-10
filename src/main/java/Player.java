a0d9c60b84ab684bee2279fc72cc4e0334fe7373
Keeano Gerald <keeanogerald@gmail.com>
2023-02-10 02:23:16 -0500

    Added C command to print out board

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -4,12 +4,21 @@ public class Player{
     String pen;
     String orientation;
 
-    Player(int vertPos, int horzPos, String pen, String orientation)
+    int[][] board;
+
+    int arraySize;
+
+    boolean isPlayerCreated;
+
+    Player(int vertPos, int horzPos, String pen, String orientation, int board[][], int arraySize, boolean isPlayerCreated)
     {
         this.vertPos = vertPos;
         this.horzPos = horzPos;
         this.pen = pen;
         this.orientation = orientation;
+        this.board = board;
+        this.arraySize = arraySize;
+        this.isPlayerCreated = isPlayerCreated;
     }
 
     public int getVertPos() {
@@ -42,5 +51,21 @@ public class Player{
 
     public void setOrientation(String orientation) {
         this.orientation = orientation;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(int[][] board) {
+        this.board = board;
+    }
+
+    public int getArraySize() {
+        return arraySize;
+    }
+
+    public void setArraySize(int arraySize) {
+        this.arraySize = arraySize;
     }
 }
